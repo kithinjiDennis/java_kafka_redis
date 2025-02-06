e332463342f830f7a4a872abc75e0e073b1980c0
KithinjiD <Kanana@2023@@>
2025-02-06 21:21:05 +0300

    Initialize Spring Boot project with basic structure and configuration

---
+++ jkr/src/main/java/com/jkr/jkr/JkrApplication.java
@@ -0,0 +1,13 @@
+package com.jkr.jkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkrApplication.class, args);
+	}
+
+}
---
+++ jkr/src/test/java/com/jkr/jkr/JkrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkr.jkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
